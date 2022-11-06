7310ca8d895b62664bd7aa98e5ff280102e49614
Dylan-Jacquet <102671618+Dylan-Jacquet@users.noreply.github.com>
2022-11-06 12:06:26 +0100

    :tada: Add JS

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+const btn_button1 = document.querySelector("#btn_button1");
+const btn_button2 = document.querySelector("#btn_button2");
+const btn_button3 = document.querySelector("#btn_button3");
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+
+btn_button1.addEventListener("click", ()=>{
+    button1.style.display="block";
+    button2.style.display="none";
+    button3.style.display="none";
+})
+btn_button2.addEventListener("click", ()=>{
+    button2.style.display="block";
+    button1.style.display="none";
+    button3.style.display="none";
+})
+btn_button3.addEventListener("click", ()=>{
+    button3.style.display="block";
+    button2.style.display="none";
+    button1.style.display="none";
+})
