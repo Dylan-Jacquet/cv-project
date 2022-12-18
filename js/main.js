3581b5276f0490f9635ef71e2f4aa61483cce87e
Dylan-Jacquet <dylan.jacquet@student.hepl.be>
2022-12-18 21:54:18 +0100

    🚧 In construction

--- js/main.js
+++ js/main.js
@@ -5,20 +5,21 @@ const buttonYear = document.getElementsByClassName('button-year');
 const parcoursImg = document.querySelectorAll('.parcours-img img');
 const btnMenus = document.querySelectorAll('.services');
 
-const handleEnter = function (){
+
+
+const handleEnter = function (e) {
     this.lastElementChild.classList.add('trigger-enter');
-    setTimeout(() => this.lastElementChild.classList.add('trigger-enter-active'),150);
-    this.lastElementChild.after.classList.remove('trigger-enter')
+    setTimeout(() => this.lastElementChild.classList.add('trigger-enter-active'), 150);
 }
 
-const handleExit = function (){
-    this.lastElementChild.classList.remove('trigger-enter' , 'trigger-enter-active');
+const handleExit = function () {
+    this.lastElementChild.classList.remove('trigger-enter', 'trigger-enter-active');
 }
 btnMenus.forEach(btnMenu => btnMenu.addEventListener('click', handleEnter));
 btnMenus.forEach(btnMenu => btnMenu.addEventListener('mouseenter', handleEnter));
 btnMenus.forEach(btnMenu => btnMenu.addEventListener('mouseleave', handleExit));
 //BUTTON DESCRIPTION
-const displayTab = function (e){
+const displayTab = function (e) {
     let id = e.currentTarget.dataset.number;
     for (const tabSelectorElement of tabSelector) {
         tabSelectorElement.classList.add('hidden');
@@ -30,19 +31,40 @@ const displayTab = function (e){
     document.getElementById(id).classList.remove(`hidden`);
 };
 
-for (let i = 0; i <buttonSelector.length ; i++) {
-    buttonSelector[i].addEventListener('click',(e)=>{
+for (let i = 0; i < buttonSelector.length; i++) {
+    buttonSelector[i].addEventListener('click', (e) => {
         displayTab(e);
     })
 }
 
 //BUTTON YEAR
 for (const buttonYearElement of buttonYear) {
-    buttonYearElement.addEventListener('click',(e)=>{
+    buttonYearElement.addEventListener('click', (e) => {
         let data = e.currentTarget.dataset.order;
         for (const img of parcoursImg) {
             img.classList.remove('active');
         }
         document.getElementById(data).classList.add('active')
     });
+}
+
+
+const buttonsReviews = document.getElementsByClassName('slider__navlink');
+
+
+if(window.innerWidth < 995){
+
+}
+
+for (const buttonReview of buttonsReviews) {
+    buttonReview.addEventListener('click', function (e){
+        removeClassActiveReviews(e);
+        e.currentTarget.classList.add('slider-navlink-active');
+    });
+}
+
+const removeClassActiveReviews = function (e){
+    for (const buttonsReview of buttonsReviews) {
+        buttonsReview.classList.remove('slider-navlink-active');
+    }
 }
